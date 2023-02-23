76dcb85d4e0d3fe9d506d4c90c4cadad7a951cd8
Tomohiro Matsunaga <lc21-1212@lclark.edu>
2023-02-23 11:24:21 -0800

    add find_empty, isUnsatisfied,find_unsatisfied

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,71 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import static java.lang.System.exit;
 
 public class Main {
     // initialize param
-    public static final int size = 30;
-    public static final double satisfied = 0.3;
-    public static final double rate_RED = 0.5;
-    public static final double rate_BLUE = 1 - rate_RED;
-    public static final double empty = 0.1;
-    public static int[][] map = new int[size][size]; // red -> 1 | blue -> -1 | empty -> 0
+    public static final int SIZE = 30;
+    public static final double SATISFIED = 0.3;
+    public static final double RATE_RED = 0.5;
+    public static final double RATE_BLUE = 1 - RATE_RED;
+    public static final double EMPTY = 0.1;
+    public static int[][] MAP = new int[SIZE][SIZE]; // red -> 1 | blue -> -1 | empty -> 0
 
     public static void main(String[] args) {
         // TODO: initialize map: randomly place red and blue on the map
         initialize_map();
 
         // TODO: draw the grid
-        StdDraw.setXscale(0, size);
-        StdDraw.setYscale(0, size);
+        StdDraw.setXscale(0, SIZE);
+        StdDraw.setYscale(0, SIZE);
         draw();
 
         // TODO: start simulation
-            // TODO: keep running until no more movement will happen
+            // TODO: keep running until no mor e movement will happen
 
     }
 
+    /**
+     * Draw the map
+     */
     public static void draw() {
 
     }
 
+    /**
+     * Initialize the map, randomly arrange two colors on the map
+     */
     public static void initialize_map() {
-        int numEmpty = (int) (size * size * empty);
-        int numBlue = (int) (size * size * rate_BLUE);
-        int numRed = (int) (size * size * rate_RED);
+        int numEmpty = (int) (SIZE * SIZE * EMPTY);
+        int numBlue = (int) (SIZE * SIZE * RATE_BLUE);
+        int numRed = (int) (SIZE * SIZE * RATE_RED);
 
-        int countEmpty = size * size;
+        int countEmpty = SIZE * SIZE;
         int countBlue = 0;
         int countRed = 0;
 
         // put blue on map
         for (int i = 0; i < numBlue; ++i) {
             while (true) {
-                int x = StdRandom.uniformInt(map.length);
-                int y = StdRandom.uniformInt(map.length);
-                if (map[x][y] == 0) {
-                    map[x][y] = -1;
+                int x = StdRandom.uniformInt(MAP.length);
+                int y = StdRandom.uniformInt(MAP.length);
+                if (MAP[x][y] == 0) {
+                    MAP[x][y] = -1;
                     countBlue++;
                     countEmpty--;
                     break;
                 }
             }
         }
-        
+
         // put red on map
         for (int i = 0; i < numRed; ++i) {
             while (true) {
-                int x = StdRandom.uniformInt(map.length);
-                int y = StdRandom.uniformInt(map.length);
-                if (map[x][y] == 0) {
-                    map[x][y] = 1;
+                int x = StdRandom.uniformInt(MAP.length);
+                int y = StdRandom.uniformInt(MAP.length);
+                if (MAP[x][y] == 0) {
+                    MAP[x][y] = 1;
                     countRed++;
                     countEmpty--;
                     break;
@@ -71,5 +80,43 @@ public class Main {
         } else {
             System.out.println("initialization successful");
         }
+    }
+
+    /**
+     * Returns an empty position coordinate
+     * @return An array of length 2, representing X & Y
+     */
+    public static int[] find_empty() {
+        while(true) {
+            int x = StdRandom.uniformInt(MAP.length);
+            int y = StdRandom.uniformInt(MAP.length);
+            if (MAP[x][y] == 0) return new int[]{x, y};
+        }
+    }
+
+    /**
+     * Returns true if the agent is satisfied; otherwise returns false
+     * Satisfactory condition: at least 30% (@param SATISFIED) of the neighbors are also the same color
+     * @param x X coordinate
+     * @param y Y coordinate
+     * @return A boolean value represents whether the satisfaction
+     */
+    public static boolean isUnsatisfied(int x, int y) {
+        int count_same = 0, amount = 0;
+
+        for (int y2 = y - 1; y2 <= y + 1; ++y2) {
+            for (int x2 = x - 1; x2 <= x + 1; ++x2) {
+                if (y2 >= 0 && y2 < MAP.length && x2 >= 0 && x2 < MAP.length) {
+                    if (MAP[x][y] == MAP[x2][y2]) count_same++;
+                    amount++;
+                }
+            }
+        }
+
+        return (double) (count_same / amount) >= SATISFIED;
+    }
+
+    public static ArrayList<Array> find_unsatisfied() {
+
     }
 }
